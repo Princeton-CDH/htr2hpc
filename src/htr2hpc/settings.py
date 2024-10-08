38d69630bb01ebcb4e79e629225b97cff35abba0
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-10-08 10:47:34 -0400

    Configure htr2hpc templates dir and cas login redirect behavior

--- src/htr2hpc/settings.py
+++ src/htr2hpc/settings.py
@@ -1,4 +1,10 @@
-from escriptorium.settings import INSTALLED_APPS
+from pathlib import Path
+
+from escriptorium.settings import INSTALLED_APPS, TEMPLATES, LOGIN_REDIRECT_URL
+
+# base directory for this package where it is installed
+HTR2HPC_INSTALL_DIR = Path(__file__).parent
+
 
 INSTALLED_APPS += [
     "django_cas_ng",
@@ -25,9 +31,15 @@ PUCAS_LDAP = {
 # use local url config
 ROOT_URLCONF = "htr2hpc.urls"
 
-# TODO:  put htr2hpc template path in first so it will override
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(PROJECT_ROOT, 'templates'),
-#                  os.path.join(BASE_DIR, 'homepage')],
+# Insert local templates path first so it will take precedence
+TEMPLATES[0]["DIRS"].insert(0, HTR2HPC_INSTALL_DIR / "templates")
+# NOTE: we may eventually include this package as an installed app,
+# in which case custom templates would be picked up via app dirs;
+# But to override escriptorium templates, we need to treat it as a
+# template directory and put it first in the list.
+
+# default django-cas behavior is to redirect back to the referrer,
+# which puts you at the login page; redirect instead to escriptorium default,
+# which is currently configured as the projects list page
+CAS_REDIRECT_URL = LOGIN_REDIRECT_URL
+CAS_IGNORE_REFERER = True

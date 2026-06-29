06dbb89b90fceeb884731d8f7c278602b741637e
hao <97079365+tanhaow@users.noreply.github.com>
2026-06-29 16:02:11 -0400

    add Sphinx documentation source files

---
+++ sphinx-docs/codedocs.rst
@@ -0,0 +1,47 @@
+Code Documentation
+==================
+
+.. toctree::
+   :maxdepth: 2
+
+htr2hpc
+-------
+
+.. automodule:: htr2hpc
+   :members:
+
+API Client
+----------
+
+.. automodule:: htr2hpc.api_client
+   :members:
+
+Tasks
+-----
+
+.. automodule:: htr2hpc.tasks
+   :members:
+
+Views
+-----
+
+.. automodule:: htr2hpc.views
+   :members:
+
+Training
+--------
+
+.. automodule:: htr2hpc.train.run
+   :members:
+
+.. automodule:: htr2hpc.train.data
+   :members:
+
+.. automodule:: htr2hpc.train.slurm
+   :members:
+
+.. automodule:: htr2hpc.train.calculate
+   :members:
+
+.. automodule:: htr2hpc.train.rm_models
+   :members:
---
+++ sphinx-docs/conf.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from unittest.mock import MagicMock
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
+
+# Mock modules that require eScriptorium or other unavailable dependencies
+# so autodoc can import htr2hpc modules without a full eScriptorium install.
+for _mod in [
+    "apps",
+    "apps.users",
+    "apps.users.consumers",
+    "celery",
+    "channels",
+    "channels.generic",
+    "channels.generic.websocket",
+    "channels.layers",
+    "coremltools",
+    "coremltools.models",
+    "coremltools.models.MLModel",
+]:
+    sys.modules.setdefault(_mod, MagicMock())
+
+import django  # noqa: E402
+from django.conf import settings  # noqa: E402
+
+if not settings.configured:
+    settings.configure(
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sites",
+        ],
+        DATABASES={"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}},
+        SITE_ID=1,
+    )
+    django.setup()
+
+from htr2hpc import __version__  # noqa: E402
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+master_doc = "index"
+
+project = "htr2hpc"
+copyright = "2025, CDH @ Princeton University"
+author = "CDH @ Princeton University"
+
+version = __version__
+release = __version__
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+pygments_style = "sphinx"
+todo_include_todos = True
+
+html_theme = "alabaster"
+html_theme_options = {
+    "description": "HTR training on HPC for eScriptorium",
+    "github_user": "Princeton-CDH",
+    "github_repo": "htr2hpc",
+    "github_button": True,
+    "badge_branch": "main",
+}
+html_sidebars = {
+    "**": [
+        "about.html",
+        "navigation.html",
+        "localtoc.html",
+        "searchbox.html",
+    ],
+}
+html_static_path = ["_static"]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}

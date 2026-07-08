9d350dde04b8fd335e26b81be8f16775a19d1b62
hao <97079365+tanhaow@users.noreply.github.com>
2026-07-08 01:17:19 -0400

    update the label name to the production url

--- src/htr2hpc/train/user_setup.sh
+++ src/htr2hpc/train/user_setup.sh
@@ -41,7 +41,7 @@ if $ssh_setup; then
 	fi
 
 	# add htr2hpc public key to authorized keys if not already present (same key used for staging and production)
-	ssh_key='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzoR8jstrofzFKVoiXSFP5jGw/WbXHxFyIaS5b4vSWC htr2hpc'
+	ssh_key='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzoR8jstrofzFKVoiXSFP5jGw/WbXHxFyIaS5b4vSWC htr.cdh.princeton.edu'
 	if ! grep -q "$ssh_key" $HOME/.ssh/authorized_keys; then
 	    echo "Adding htr2hpc ssh key to authorized keys"
 	    echo $ssh_key >> ~/.ssh/authorized_keys

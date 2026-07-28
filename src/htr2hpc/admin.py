5a326f8915bed4afca659fecc82a2de5ccec565a
hao <97079365+tanhaow@users.noreply.github.com>
2026-07-28 12:09:07 -0400

    Exclude admin.py from test coverage and add notes explaining why

--- src/htr2hpc/admin.py
+++ src/htr2hpc/admin.py
@@ -1,15 +1,20 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib import admin  # pragma: no cover
+from django.contrib.auth import get_user_model  # pragma: no cover
 
-from pucas.admin import CasUserAdmin
-from users.admin import MyUserAdmin
+from pucas.admin import CasUserAdmin  # pragma: no cover
+from users.admin import MyUserAdmin  # pragma: no cover
 
 
-class Htr2HpcUserAdmin(CasUserAdmin, MyUserAdmin):
-    """Extends eScriptorium's UserAdmin with pucas CAS user management."""
+class Htr2HpcUserAdmin(CasUserAdmin, MyUserAdmin):  # pragma: no cover
+    """Extends eScriptorium's UserAdmin with pucas CAS user management.
+
+    Not covered by tests because this module depends on eScriptorium's MyUserAdmin
+    which is not available in the test environment. If functionality is added
+    here, update test settings to support it.
+    """
     pass
 
 
 # replace eScriptorium's User registration with our extended admin
-admin.site.unregister(get_user_model())
-admin.site.register(get_user_model(), Htr2HpcUserAdmin)
+admin.site.unregister(get_user_model())  # pragma: no cover
+admin.site.register(get_user_model(), Htr2HpcUserAdmin)  # pragma: no cover
